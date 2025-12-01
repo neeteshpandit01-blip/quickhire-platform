9326661eb1dd1f74f4dbb5d361b5794edafdf45b
neeteshpandit01-blip <neeteshpandit01@gmail.com>
2025-12-01 17:40:14 +0530

    feat: Add MainLayout component

---
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+
+const MainLayout = () => {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      <Box component="main" sx={{ flexGrow: 1, py: 3 }}>
+        <Outlet />
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default MainLayout;
